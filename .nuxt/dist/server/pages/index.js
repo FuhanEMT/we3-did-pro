add7b1e043f773a983de9b4f484f37bb3a14c39c
FuhanEMT <625165799@qq.com>
2022-11-14 21:06:46 +0800

    fix: 初始化

---
+++ .nuxt/dist/server/pages/index.js
@@ -0,0 +1,212 @@
+exports.ids = [1];
+exports.modules = {
+
+/***/ 24:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "img/logo.c5b77ac.png";
+
+/***/ }),
+
+/***/ 25:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "img/search.16e0551.png";
+
+/***/ }),
+
+/***/ 27:
+/***/ (function(module, exports, __webpack_require__) {
+
+// style-loader: Adds some css to the DOM by adding a <style> tag
+
+// load the styles
+var content = __webpack_require__(38);
+if(content.__esModule) content = content.default;
+if(typeof content === 'string') content = [[module.i, content, '']];
+if(content.locals) module.exports = content.locals;
+// add CSS to SSR context
+var add = __webpack_require__(6).default
+module.exports.__inject__ = function (context) {
+  add("eecd31ba", content, true, context)
+};
+
+/***/ }),
+
+/***/ 30:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "img/i18.32e3dd3.png";
+
+/***/ }),
+
+/***/ 31:
+/***/ (function(module, exports) {
+
+module.exports = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACQAAAAVCAYAAAAuJkyQAAAAAXNSR0IArs4c6QAAAmhJREFUSEu11cnLjWEcxvEPMpM5f4ANC3tbO0nJkFggImNEiDJbmIpkDgkLkiElltbWFlb+AJmSmQxduY/uHt7hvK/3qtM553nu+/59n99wPf38Vj9Mwmycx/tyva+/RmAV7uEZfgYkn2k4hSk4jf1418c0I7Eb6/C0fD8OzGTcwFT0xydcwoYQ9xFU4p7EcgzFDzzBwtzYgn0Y1gh+Dtvx9j9DjcIhrG6c+xF7AjS8BN7cgPqMC9j1H6ECcwArMaQCCsyxgAYoCtS2UtMaPFDXywHfe5mpAeUBFzVgcmx69gg+tIBasdJkAQtgrSvYhDc9hBqD41ja2P8Bh0vW/ox7vSZ9lMDpnYxkS18QqB143SbUWBwsMIOrvbGW9FJAU7J/AuVioDKKSWGtr7iDJfjWTaiBuIq5GNTYk0rEYv7A5H6zZPWeNPlexC9q3SzAL7uAGl8CLmisi7/l3DTxX+oMKP6wNqPYgEqm0uhb8aIDqAk4ijRwnZnAxGLOFL9rCyiLA7WsuHgNn5I9iJEhk1gr4xyjnYmUrKWY7Hpc7gimq5LVQWJiacD4SK37WIHn5eJEXMSsxrqYawYlZtupOitZ86kTOKY2urqRTN1G+i1KX8xrZCYwOwtoM5ttl6zekJFNTyQDMbmWAvWo/JnegImZ5kHSc7GOLtXdDNUHLcYJxOw6U0x0I651SVEt6AlQMhWo9NS4DoK9Kj0TmG5lpnVOT4CyN6M8p5he0/BiCzHPu8jvttRToFaQ+TiLmGAUs1yDW21R9LJkdaxkJ14UE4xilvGgtjPT25LVUDG/GeU19LCN99w/k/gLXtB2vrtbPc8AAAAASUVORK5CYII="
+
+/***/ }),
+
+/***/ 32:
+/***/ (function(module, exports) {
+
+module.exports = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEgAAABICAYAAABV7bNHAAAAAXNSR0IArs4c6QAAAm9JREFUeF7tnDuLFEEUhb8DopnCgkai4B/Q37CpmGgoYiQmxgomIguyG5m4gRiJGBiIgZiICBqIoYYmggviY/G1CL4puTLKTDvd1VqTTNepcPpB19fnnlt1u2qEWycBmU83AQPKKMSADKjMRKwgK8gKKiNgBZXxm/CglNJW4ARwHNhVduu5u3oNuAhckLTx++mbgE4DZ4FNc9e92Tzwd+CMpHNtgJ5VqJwm2jVJu9sApdm8iPm+i6Q/kdUMMQMCDCiX2q2gzAzegAyoKEvYg+xBRQJyFsvhc4g5xHIacRYrIuQQ68b3XNJOT1anQ/oGLElaMqC/Ab0BzgOrkt7XDugH8AF4AdwGbgAPJMXvE63GcscT4BZwB3go6V2XJdUE6C2wDFwDXkn60ifV1QDoK3APOAk8lvRPRcGhAwqzXQ3lSPrYRzE1edDnSNmRmSR9+h84cc2QFXQf2N9UTkop+rwN2A5sHgMXobge2W08DIcM6KCkSN8TLaW0B1gBFoGFsYNh4neBU5Ke1jAOWpiWwlNKl4EjzegZAQkDvyLpaM2AXo/Cq82W1iXtqAFQW4g9AvZ2mHYMBfbVAKjNpA8BV4EtUyDF4PGwpOs1AJqa5lNKsTDjAHCssQ4hVndcAm5KikUMv9qQs1j0zwPFHgNATzV6QIpTPFntCcrljh6gojAW/vTSBbMetEanuOTag5WL9j0g+bNPDpI/HGYIGZAB5YKo+7gVZAVZQWUErKAyfvYgK8gKKiNgBZXx6/Igb6iDzg113pKZ2ZLpTb1dm3rLIneYV/uvKXIZbZjvfXa9soKsoDI1WUFWkBVURsAKKuP3Ez/IU1hH0ogoAAAAAElFTkSuQmCC"
+
+/***/ }),
+
+/***/ 33:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "img/banner.0e192f4.png";
+
+/***/ }),
+
+/***/ 34:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "img/show.17f2c89.png";
+
+/***/ }),
+
+/***/ 35:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "img/antins.df24769.png";
+
+/***/ }),
+
+/***/ 36:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "img/footer.2635f47.png";
+
+/***/ }),
+
+/***/ 37:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_vue_style_loader_index_js_ref_5_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_5_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_5_oneOf_1_2_node_modules_less_loader_dist_cjs_js_ref_5_oneOf_1_3_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_index_vue_vue_type_style_index_0_id_22ab180d_prod_lang_less_scoped_true___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(27);
+/* harmony import */ var _node_modules_vue_style_loader_index_js_ref_5_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_5_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_5_oneOf_1_2_node_modules_less_loader_dist_cjs_js_ref_5_oneOf_1_3_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_index_vue_vue_type_style_index_0_id_22ab180d_prod_lang_less_scoped_true___WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_vue_style_loader_index_js_ref_5_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_5_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_5_oneOf_1_2_node_modules_less_loader_dist_cjs_js_ref_5_oneOf_1_3_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_index_vue_vue_type_style_index_0_id_22ab180d_prod_lang_less_scoped_true___WEBPACK_IMPORTED_MODULE_0__);
+/* harmony reexport (unknown) */ for(var __WEBPACK_IMPORT_KEY__ in _node_modules_vue_style_loader_index_js_ref_5_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_5_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_5_oneOf_1_2_node_modules_less_loader_dist_cjs_js_ref_5_oneOf_1_3_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_index_vue_vue_type_style_index_0_id_22ab180d_prod_lang_less_scoped_true___WEBPACK_IMPORTED_MODULE_0__) if(["default"].indexOf(__WEBPACK_IMPORT_KEY__) < 0) (function(key) { __webpack_require__.d(__webpack_exports__, key, function() { return _node_modules_vue_style_loader_index_js_ref_5_oneOf_1_0_node_modules_css_loader_dist_cjs_js_ref_5_oneOf_1_1_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_5_oneOf_1_2_node_modules_less_loader_dist_cjs_js_ref_5_oneOf_1_3_node_modules_nuxt_components_dist_loader_js_ref_0_0_node_modules_vue_loader_lib_index_js_vue_loader_options_index_vue_vue_type_style_index_0_id_22ab180d_prod_lang_less_scoped_true___WEBPACK_IMPORTED_MODULE_0__[key]; }) }(__WEBPACK_IMPORT_KEY__));
+
+
+/***/ }),
+
+/***/ 38:
+/***/ (function(module, exports, __webpack_require__) {
+
+// Imports
+var ___CSS_LOADER_API_IMPORT___ = __webpack_require__(5);
+var ___CSS_LOADER_EXPORT___ = ___CSS_LOADER_API_IMPORT___(false);
+// Module
+___CSS_LOADER_EXPORT___.push([module.i, ".box[data-v-22ab180d]{width:100%}.header[data-v-22ab180d]{max-width:1210px;min-width:1000px;padding:65px 0;margin:0 auto;display:flex;align-items:center;justify-content:space-between}.header .right-secretKey[data-v-22ab180d]{display:flex;align-items:center;font-weight:700}.header .right-secretKey .i18-logo[data-v-22ab180d]{width:23px;margin-right:5px}.header .right-secretKey .i18-logo img[data-v-22ab180d]{width:100%}.header .right-secretKey .i18-bottom[data-v-22ab180d]{margin-left:5px;width:15px;margin-right:5px}.header .right-secretKey .i18-bottom img[data-v-22ab180d]{width:100%}.header .center-assgin[data-v-22ab180d]{display:flex;align-items:center}.header .center-assgin div[data-v-22ab180d]{color:#000;font-size:16px;margin-right:60px;font-weight:700}.header .right-connect[data-v-22ab180d]{display:flex;background:#000;font-size:14px;color:#fff;padding:5px 10px;align-items:center;border-radius:5px}.header .right-connect .icon[data-v-22ab180d]{width:20px;margin-right:10px}.header .right-connect .icon img[data-v-22ab180d]{width:100%}.header .logo[data-v-22ab180d]{width:100px}.header .logo img[data-v-22ab180d]{width:100%}.nav_size[data-v-22ab180d]{margin:25px 0}.seach-box[data-v-22ab180d]{width:100%;display:flex;align-items:center}.seach-box .input[data-v-22ab180d]{position:relative;background:#e5e5e5;width:80%}.seach-box .input .size[data-v-22ab180d]{position:absolute;right:20px;top:-4px;font-size:10px}.seach-box .input img[data-v-22ab180d]{width:23px;position:absolute;top:21px;left:20px;z-index:1}.seach-box .input input[data-v-22ab180d]{width:100%;background:#e5e5e5;border:0;padding:20px 80px 20px 20px;text-indent:40px;font-size:16px}.seach-box .btn[data-v-22ab180d]{width:20%;background:#000;color:#fff;padding:21.5px 10px;text-align:center}.content[data-v-22ab180d]{width:1210px;display:flex;align-items:center;justify-content:space-between;flex-wrap:wrap;margin:0 auto}.content .left[data-v-22ab180d]{width:50%}.content .left .banner[data-v-22ab180d]{width:600px}.content .left .banner img[data-v-22ab180d]{width:100%}.content .left .size[data-v-22ab180d]{font-size:20px;margin-top:20px}.content .left .item-ilb[data-v-22ab180d]{width:100%;display:flex;align-items:center;justify-content:space-between;margin:40px 0}.content .left .item-ilb .item[data-v-22ab180d]{width:23.5%;text-align:center;border-radius:5px;border:1px solid #e5e5e5;padding:5px 10px}.content .right[data-v-22ab180d]{width:50%;text-align:right;position:relative}.content .right img[data-v-22ab180d]:first-child{width:550px;position:relative;z-index:1}.content .right img[data-v-22ab180d]:nth-child(2){width:550px;position:absolute;left:-20px;top:-65px}.footer[data-v-22ab180d]{width:100%;background-color:#000;position:fixed;bottom:0;padding:40px 0 70px;z-index:2}.footer[data-v-22ab180d],.footer .box[data-v-22ab180d]{display:flex;justify-content:space-between}.footer .box[data-v-22ab180d]{width:1200px;margin:0 auto}.footer div[data-v-22ab180d]{width:40%;text-align:center}.footer .left div[data-v-22ab180d]{text-align:left;font-size:10px;color:#fff;width:500px;margin:0 auto}.footer .left div img[data-v-22ab180d]{width:150px;margin-bottom:20px}.footer .right[data-v-22ab180d]{display:flex}.footer .right .item div[data-v-22ab180d]:first-child{font-weight:700;font-size:18px;margin-bottom:10px;color:#fff}.footer .right .item div[data-v-22ab180d]{color:#c4c4c4;font-size:16px}", ""]);
+// Exports
+module.exports = ___CSS_LOADER_EXPORT___;
+
+
+/***/ }),
+
+/***/ 40:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--2-0!./node_modules/vue-loader/lib/loaders/templateLoader.js??ref--6!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./pages/index.vue?vue&type=template&id=22ab180d&scoped=true&
+var render = function render() {
+  var _vm = this,
+    _c = _vm._self._c;
+  return _c('div', {
+    staticClass: "box"
+  }, [_vm._ssrNode("<div class=\"header\" data-v-22ab180d><div class=\"logo\" data-v-22ab180d><img" + _vm._ssrAttr("src", __webpack_require__(24)) + " alt data-v-22ab180d></div> <div class=\"center-assgin\" data-v-22ab180d><div data-v-22ab180d>Home</div> <div data-v-22ab180d>Twitter</div> <div data-v-22ab180d>Docs</div> <div data-v-22ab180d>Stake(soon)</div></div> <div class=\"right-secretKey\" data-v-22ab180d><div class=\"i18-logo\" data-v-22ab180d><img" + _vm._ssrAttr("src", __webpack_require__(30)) + " alt data-v-22ab180d></div> <div data-v-22ab180d>EN</div> <div class=\"i18-bottom\" data-v-22ab180d><img" + _vm._ssrAttr("src", __webpack_require__(31)) + " data-v-22ab180d></div></div> <div class=\"right-connect\" data-v-22ab180d><div class=\"icon\" data-v-22ab180d><img" + _vm._ssrAttr("src", __webpack_require__(32)) + " data-v-22ab180d></div> <span data-v-22ab180d>Connect</span></div></div> <div class=\"content\" data-v-22ab180d><div class=\"left\" data-v-22ab180d><div class=\"banner\" data-v-22ab180d><img" + _vm._ssrAttr("src", __webpack_require__(33)) + " data-v-22ab180d></div> <div class=\"size\" data-v-22ab180d>\n        Get your web3 domain, replace your complex address\n      </div> <div class=\"item-ilb\" data-v-22ab180d><div class=\"item\" data-v-22ab180d>Decentralized</div> <div class=\"item\" data-v-22ab180d>Cross-chain</div> <div class=\"item\" data-v-22ab180d>Open source</div> <div class=\"item\" data-v-22ab180d>community-driven</div></div> <div class=\"seach-box\" data-v-22ab180d><div class=\"input\" data-v-22ab180d><input placeholder=\"get your web3 name\" type=\"text\" data-v-22ab180d> <div class=\"size\" data-v-22ab180d>.web3</div> <img" + _vm._ssrAttr("src", __webpack_require__(25)) + " data-v-22ab180d></div> <div class=\"btn\" data-v-22ab180d>Search</div></div> <div class=\"nav_size\" data-v-22ab180d>.web3 = .ape / .bean / .cat / .bird...</div></div> <div class=\"right\" data-v-22ab180d><img" + _vm._ssrAttr("src", __webpack_require__(34)) + " alt data-v-22ab180d> <img" + _vm._ssrAttr("src", __webpack_require__(35)) + " alt data-v-22ab180d></div></div> <div class=\"footer\" data-v-22ab180d><div class=\"box\" data-v-22ab180d><div class=\"left\" data-v-22ab180d><div data-v-22ab180d><img" + _vm._ssrAttr("src", __webpack_require__(36)) + " data-v-22ab180d> <div data-v-22ab180d>\n            .web3 is a decentralized digital identity system that is building\n            a trust,transparency and interoperable chain for users and dApps\n          </div></div></div> <div class=\"right\" data-v-22ab180d><div class=\"item\" data-v-22ab180d><div data-v-22ab180d>Resources</div> <div data-v-22ab180d>Whitebook</div> <div data-v-22ab180d>Brand Kit</div></div> <div class=\"item\" data-v-22ab180d><div data-v-22ab180d>Developer</div> <div data-v-22ab180d>Github</div> <div data-v-22ab180d>Integration</div></div> <div class=\"item\" data-v-22ab180d><div data-v-22ab180d>Community</div> <div data-v-22ab180d>Twitter</div> <div data-v-22ab180d>Discord</div> <div data-v-22ab180d>Medium</div></div></div></div></div>")]);
+};
+var staticRenderFns = [];
+
+// CONCATENATED MODULE: ./pages/index.vue?vue&type=template&id=22ab180d&scoped=true&
+
+// EXTERNAL MODULE: external "vue"
+var external_vue_ = __webpack_require__(0);
+var external_vue_default = /*#__PURE__*/__webpack_require__.n(external_vue_);
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--2-0!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./pages/index.vue?vue&type=script&lang=js&
+
+/* harmony default export */ var lib_vue_loader_options_pagesvue_type_script_lang_js_ = (external_vue_default.a.extend({
+  name: "IndexPage",
+  data() {
+    return {
+      // web授权虚拟化对象
+      webProvider: null,
+      // 钱包地址
+      metamask: null
+    };
+  },
+  mounted() {},
+  methods: {
+    // 连接 Metamask 钱包
+    getMetamask() {
+      if (window.ethereum) {
+        this.webProvider = window.ethereum;
+        try {
+          window.ethereum.enable();
+        } catch (error) {
+          console.error("User denied account access");
+        }
+      } else if (window.web3) {
+        web3Provider = window.web3.currentProvider;
+      } else {
+        this.webProvider = new this.$asyncWebOption.web.providers.HttpProvider("http://localhost:3000");
+      }
+      // 结合rpc节点实例化虚拟web对象
+      this.$asyncWebOption.rpcWeb = new this.$asyncWebOption.web(this.webProvider);
+      this.getMetamaskWallet();
+    },
+    // 获取 Metamask 钱包地址
+    getMetamaskWallet() {
+      if (window.ethereum) {
+        window.ethereum.enable().then(res => {
+          this.metamask = res[0];
+        });
+      } else {
+        alert("请安装MetaMask钱包");
+      }
+    }
+  }
+}));
+// CONCATENATED MODULE: ./pages/index.vue?vue&type=script&lang=js&
+ /* harmony default export */ var pagesvue_type_script_lang_js_ = (lib_vue_loader_options_pagesvue_type_script_lang_js_); 
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__(2);
+
+// CONCATENATED MODULE: ./pages/index.vue
+
+
+
+function injectStyles (context) {
+  
+  var style0 = __webpack_require__(37)
+if (style0.__inject__) style0.__inject__(context)
+
+}
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  pagesvue_type_script_lang_js_,
+  render,
+  staticRenderFns,
+  false,
+  injectStyles,
+  "22ab180d",
+  "4797ee2c"
+  
+)
+
+/* harmony default export */ var pages = __webpack_exports__["default"] = (component.exports);
+
+/***/ })
+
+};;
+//# sourceMappingURL=index.js.map
